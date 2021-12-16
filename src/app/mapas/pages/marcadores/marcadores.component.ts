431eeb43cbc23c594103b024e53b2a5bd6c3a639
Leandro <leandro.n.aguilera@gmail.com>
2021-12-16 18:06:48 -0300

    Guardar y leer del LocalStorage

--- src/app/mapas/pages/marcadores/marcadores.component.ts
+++ src/app/mapas/pages/marcadores/marcadores.component.ts
@@ -3,7 +3,8 @@ import * as mapboxgl from 'mapbox-gl';
 
 interface MarcadorColor {
   color: string;
-  marker: mapboxgl.Marker;
+  marker?: mapboxgl.Marker;
+  centro?: [number, number];
 }
 
 @Component({
@@ -49,6 +50,8 @@ export class MarcadoresComponent implements AfterViewInit {
       zoom: this.zoomLevel
     });
 
+    this.leerLocalStorage();
+
     // const marker = new mapboxgl.Marker()
     //   .setLngLat( this.center )
     //   .addTo( this.mapa )
@@ -69,11 +72,54 @@ export class MarcadoresComponent implements AfterViewInit {
       color,
       marker: nuevoMarcador,
     })
+
+    this.guardarMarcadoresLocalStorage();
   }
 
   irMarcador( marker: mapboxgl.Marker) {
     this.mapa.flyTo({
       center: marker.getLngLat()
+    })
+  }
+
+  guardarMarcadoresLocalStorage() {
+
+    const lngLatArr: MarcadorColor[] = [];
+
+    this.marcadores.forEach( m => {
+
+      const color = m.color;
+      const {lng, lat} = m.marker!.getLngLat();
+
+      lngLatArr.push({
+        color: color,
+        centro: [ lng, lat ]
+      });
+
+      localStorage.setItem('marcadores', JSON.stringify(lngLatArr));
+
+    })
+  }
+
+  leerLocalStorage() {
+    if ( !localStorage.getItem('marcadores')) {
+      return;
+    }
+    const lngLatArr: MarcadorColor[] = JSON.parse(localStorage.getItem('marcadores')!);
+
+    lngLatArr.forEach( m => {
+      const newMarker = new mapboxgl.Marker({
+        color: m.color,
+        draggable: true
+      })
+        .setLngLat( m.centro! )
+        .addTo( this.mapa );
+
+        this.marcadores.push({
+          marker: newMarker,
+          color: m.color
+        });
+
     })
   }
 }
